4227a1f772061f2ed8e7e6adc5b7996233677afc
wfd2018 <warmthlover@outlook.com>
2021-12-07 12:20:41 -0600

    Add files via upload

---
+++ sprite.js
@@ -0,0 +1,12 @@
+/* const ajax = new XMLHttpRequest();
+ajax.open('GET', 'https://codepen.io/Daidala/pen/MWvVVyG  ', true);
+ajax.send();
+ajax.onload = function () {
+  const div = document.createElement('div');
+  div.className = 'svgSprite';
+  div.innerHTML = ajax.responseText;
+  document.body.insertBefore(div, document.body.childNodes[0]);
+};
+
+// ajax.open("GET", "https://assets.codepen.io/13090/sprite_1.svg", true);
+// true is for async */
